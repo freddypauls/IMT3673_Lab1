2b809506b8c834fba2eb6e31116aad7977287c3d
freddypauls <paasan-fred@hotmail.com>
2018-02-14 17:34:42 +0100

    Start

---
+++ app/src/main/java/no/ntnu/fredrik/lab1/A1.java
@@ -0,0 +1,29 @@
+package no.ntnu.fredrik.lab1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Spinner;
+
+public class A1 extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a1);
+    }
+
+    public void sendMessage(View view) {
+
+        Intent intent = new Intent(this, A2.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+
+    }
+}
+
