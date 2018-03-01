715fe03f706ef77d15260984414e6876cab7f995
freddypauls <paasan-fred@hotmail.com>
2018-03-01 22:24:34 +0100

    9
    cleanup

--- app/src/main/java/no/ntnu/fredrik/lab1/A2.java
+++ app/src/main/java/no/ntnu/fredrik/lab1/A2.java
@@ -14,10 +14,10 @@ public class A2 extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        //String message = intent.getStringExtra(A1.EXTRA_MESSAGE);
+        String message = intent.getStringExtra("name");
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
-        textView.setText("Hello " + getIntent().getStringExtra("name"));
+        textView.setText("Hello " + message);
     }
 }
