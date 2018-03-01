26786b37b35358beff461337684f8eb3d67ce17b
freddypauls <paasan-fred@hotmail.com>
2018-03-01 13:16:12 +0100

    6

--- app/src/main/java/no/ntnu/fredrik/lab1/A1.java
+++ app/src/main/java/no/ntnu/fredrik/lab1/A1.java
@@ -1,20 +1,34 @@
 package no.ntnu.fredrik.lab1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.AdapterView;
 
 public class A1 extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "no.ntnu.fredrik.lab1";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a1);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_items, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        
     }
 
     public void sendMessage(View view) {
